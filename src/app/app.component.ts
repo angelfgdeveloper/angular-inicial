323b13ed9a45dd026fb6ede860d2e3ce12cd3517
Luis Angel <angelfgdeveloper@gmail.com>
2022-03-17 00:09:05 -0600

    Uso de String interpolation, property binding, eventos & ngModel con modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Angel';
+  age = 22;
+  image = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Luis',
+    age: 26,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toogleButton() {
+    this.btnDisabled = !this.btnDisabled
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
