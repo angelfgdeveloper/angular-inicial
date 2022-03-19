c87be9f54961fd2be20e46216be5d075911c91d9
Luis Angel <angelfgdeveloper@gmail.com>
2022-03-18 23:40:06 -0600

    uso de *ngIf, *ngFor con arrays, *ngSwitch, estilos - class, style, ngClass ngStyle y formularios, etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,77 @@
 import { Component } from '@angular/core';
+import { Product } from '../app/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Angel';
   age = 22;
   image = 'https://source.unsplash.com/random';
   btnDisabled = true;
+
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
   person = {
     name: 'Luis',
     age: 26,
-    avatar: 'https://source.unsplash.com/random'
+    avatar: 'https://source.unsplash.com/random',
   }
 
+  names: string[] = ['Eve', 'Soto', 'Margarita'];
+  newName = '';
+
+  emojis = ['😂', '🐦', '🐳', '🌮', '💚'];
+
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  }
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg',
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg',
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg',
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg',
+    },
+  ];
+
   toogleButton() {
-    this.btnDisabled = !this.btnDisabled
+    this.btnDisabled = !this.btnDisabled;
   }
 
   increaseAge() {
@@ -32,5 +86,18 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
+
+  onRegister() {
+    console.log(this.register);
   }
 }
